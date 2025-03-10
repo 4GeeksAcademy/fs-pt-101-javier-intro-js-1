9d4c131c4de6363d87aa10545ca75b877c00a75f
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2025-03-10 19:54:07 +0000

    intro a js 2

--- src/app.js
+++ src/app.js
@@ -164,7 +164,7 @@ if (age > 20) {
 }
 else if (age > 15) {
   console.log('puedes manejar ---- 1111')
-} 
+}
 else {
   console.log('coche de control remoto ---- 111')
 }
@@ -174,13 +174,317 @@ if (age > 20) {
 }
 if (age > 15) {
   console.log('puedes manejar ---- 1111')
-} 
+}
 
 /*
-
 trabajos con arrays
 loops
 trabajos con objetos
 funciones
+*/
+
+const arraOne = [5, 8, 4, 6, 85, 545, 215, 31285, 6854, 2121, 51, 2, 5, 1, 3]
+const testing = arraOne
+console.log(arraOne);
+console.log(testing);
+testing[0] = 111111111111111
+console.log('one ---> ', arraOne);
+console.log('testing ---> ', testing);
+
+
+const testArra = [...arraOne] //spread operator--> crea una copia exacta del array en OTRA seccion de memoria
+console.log('testArra ------> ', testArra)
+console.log('one', arraOne);
+console.log('test', testArra);
+testArra[0] = 555555
+console.log('one', arraOne);
+console.log('test', testArra);
+
+
+
+console.log(arraOne)
+console.log(arraOne.length)
+console.table(arraOne)
+console.log(arraOne[0])
+console.log(arraOne[10])
+console.log(arraOne[arraOne.length - 1])
+arraOne[0] = 5000 //modifica un elemento pero necesitamos saber el indice, modifica el array
+console.log(arraOne)
+
+arraOne.push(88888) //modifica el array
+arraOne.push(7777, 99999) //modifica el array
+arraOne.push('pepe') //modifica el array
+console.log(arraOne)
+let test1 = arraOne.pop() //modifica el array
+console.log(arraOne)
+console.log(test1);
+
+arraOne.shift() // elimina primera entrada del array
+console.log(arraOne)
+arraOne.unshift('pepe', 'lola') // añade lo que le pasemos al principio del array
+console.log(arraOne)
+
+const newArray = arraOne.slice(3, 8)
+console.table(arraOne)
+console.table(newArray)
+
+arraOne.splice(5, 8) // ojo! esto modifica el array
+console.log(arraOne)
+arraOne.splice(0, 1, 'matia') // desde la posicion 0, elimina uno y añade a matia en esa posicion
+console.log(arraOne)
+arraOne.splice(0, 1, 'pepe', 'el', 'magnifico') // el retorno de pepe
+console.log(arraOne)
+arraOne.splice(4, 0, 'la', 'suprema') // lola no se queda a tras
+console.log(arraOne)
+
+// para no modificar array original toSpliced() --> devuelve un array nuevo
+const arraySpliced = arraOne.toSpliced(0, 7, 'soy', 'lo', 'maximo', 'firma', 'matia')
+console.log(arraySpliced)
+console.log(arraOne)
+
+/*
+
+el splice modifica el array en el que se ejecuta
+toSpliced() va a devolver un nuevo array
 
 */
+
+
+// loops (ciclos, itinerancia)
+//algo que se repite....
+
+console.log('testArra ------> ', testArra)
+console.log('testArra.length ------> ', testArra.length)
+// [5,8,4,6,85,545,215,31285,6854,2121,51,2,5,1,3]
+
+//clasico
+for (let i = 0; i < testArra.length; i++) {
+  console.log('i--->', i);
+  console.log('valores ---> ', testArra[i])
+}
+
+// for of / for in
+// for of accede a valores
+for (let num of testArra) {
+  console.log('for of... ---> ', num)
+}
+
+// for in devuelve indices 
+for (let i in testArra) {
+  console.log('for in... ---> ', i)
+  console.log('for in valores ---> ', testArra[i])
+}
+
+// while --> es facilmente un loop infinito, SE TIENE QUE PONER MANUALMENTE DENTRO 
+//           DEL BLOQUE DE CODIGO EL INCREMENTADOR
+// do while --> es facilmente un loop infinito, SE TIENE QUE PONER MANUALMENTE DENTRO 
+//           DEL BLOQUE DE CODIGO EL INCREMENTADOR
+//            SIEMPRE SE EJECUTA AL MENOS UNA VEZ
+// switch
+
+
+/*
+let i = 0
+while (i<5) {
+//codigo que se ejecuta
+i++
+}
+
+
+let i = 0
+do {
+// bloque de codigo
+i++
+} while (condicion)
+
+
+*/
+
+let colors = 'red'
+switch (colors) {
+  case 'red': // 
+    console.log('color is red')
+    break
+  case 'blue':
+    console.log('color is blue')
+    break;
+  case 'olive':
+  case 'green':
+    console.log('color is green')
+    break; // para que no siga devolviendo 
+  default: // si no se encuentra en los case, se devuelve el default
+    console.log('color is black')
+    break;
+}
+
+let num = 5
+// switch (num) {
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     console.log('es menor a 5')
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log('es mayor a 5')
+//     break
+//   default:
+//     console.log('es 5')
+// }
+
+
+// switch(true){
+//   case num<5:
+//     console.log('menor que 5');
+//     break
+//   case num>5:
+//     console.log('mayor que 5');
+//     break
+//   default:
+//     console.log('es 5');
+// }
+
+// objetos!
+
+/*
+definidos por {}
+ordenados en pares llave: valor
+describir algo
+*/
+
+const testObj = {
+  name: 'pepe'
+}
+
+console.log(testObj)
+console.table(testObj)
+
+testObj.age = 25 // añadiendo una propiedad/llave nueva con un valor
+testObj.aeg = 25
+delete testObj.aeg //elimina una propiedad/llave 
+testObj['city'] = 'Sevilla' // añadiendo una propiedad/llave nueva con un valor
+console.log(testObj)
+testObj.car = true
+console.log(testObj.car)
+
+let testingObj = 'pais'
+testObj[testingObj] = 'España'
+console.log(testObj)
+
+testObj.luckyNumbers = [5,7,23]
+testObj.contact = {
+  name: 'lola',
+  phone: '+34 6666666669',
+  schedule: {
+    morning: '9-13',
+    night: '19-02'
+  }
+}
+
+console.log(testObj)
+console.log(testObj.luckyNumbers[2]) //23
+testObj.luckyNumbers[2]=3
+
+console.log(testObj.luckyNumbers[2]) //3
+testObj.luckyNumbers.pop()
+console.log(testObj.luckyNumbers) 
+
+console.log(testObj.contact.name) // lola
+console.log(testObj.contact.schedule.morning) // 9-13
+testObj.contact.schedule.morning = '10-14'
+console.log(testObj.contact.schedule.morning) // 10-14
+console.log(testObj)
+
+//funciones
+
+/*
+
+funcion clasica
+
+declaracion
+
+function nombreDeFuncion (params) {
+//bloque de codigo
+return (
+//lo que devuelve la funcion ---> SIEMPRE DEBE DEVOLVER ALGO
+)
+}
+
+
+funcion flecha
+
+declaracion
+
+const arrowFunction = (params) => {
+  //bloque de codigo
+return (
+//lo que devuelve la funcion ---> SIEMPRE DEBE DEVOLVER ALGO
+)
+}
+
+
+ejecucion de una funcion
+
+nombreDeFuncion() ---> el () es el que le dice a JS que ejecute la funcion
+arrowFunction()
+
+
+
+EL CONSOLE LOG, ES O EN LA FUNCION O EN EL LLAMADO A LA FUNCION
+*/
+
+
+const sumV1 = () => {
+  let total = 2+3
+  return total
+}
+console.log(sumV1())
+
+
+const sayHi = () => {
+  return 'Hola!'
+}
+
+console.log(sayHi())
+
+
+
+const sumV2 = (num) => {
+  let total = num+num
+  return total
+}
+console.log(sumV2(80))
+
+
+const sayHiV2 = (name) => {
+  //return 'Hola ' + name + '!'
+  return `Hola ${name}!`
+}
+
+console.log(sayHiV2('Lola'))
+
+
+const sumV3 = (num1, num2) => {
+  console.log('num1 -> ', num1, 'num2 -> ', num2)
+  let total = num1+num2
+  return total
+}
+console.log(sumV3(5,4))
+
+
+const sayHiV3 = (name, lastname) => {
+  //return 'Hola ' + name + '!'
+  return `Hola ${name} ${lastname}!`
+}
+
+console.log(sayHiV3('Lola', 'Paredes'))
+
+const sumV4 = (num1, num2) => num1+num2
+
+console.log(sumV4(5,4))
+
+const sayHiV4 = (name, lastname) => `Hola ${name} ${lastname}!`
+
+console.log(sayHiV4('Lola', 'Paredes'))
